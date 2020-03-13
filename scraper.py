17341bf278cd01d6f0fd8432515a85446614a798
neeraj1bh <neeraj1bh@gmail.com>
2020-03-13 23:33:37 +0530

    Scraper title.

--- scraper.py
+++ scraper.py
@@ -1,1 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
 
+# Enter the amazon URL below
+URL = ''
+
+# Enter your user agent here 'Search google for My User Agent'
+headers = {}
+
+page = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+title = soup.find(id="productTitle").get_text()
+price = soup.find(id="priceblock_ourprice").get_text()
+
+print(title.strip())
