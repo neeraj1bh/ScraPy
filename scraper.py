5bf33ec942a1179744ce3f1df628c9d8b3d9cdef
neeraj1bh <neeraj1bh@gmail.com>
2020-03-14 09:57:35 +0530

    Added email functionality.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
 # Enter the amazon URL below
 URL = ''
@@ -7,11 +8,47 @@ URL = ''
 # Enter your user agent here 'Search google for My User Agent'
 headers = {}
 
-page = requests.get(URL, headers=headers)
+def check_price():
+	page = requests.get(URL, headers=headers)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+	soup = BeautifulSoup(page.content, 'html.parser')
 
-title = soup.find(id="productTitle").get_text()
-price = soup.find(id="priceblock_ourprice").get_text()
+	title = soup.find(id="productTitle").get_text()
+	price = soup.find(id="priceblock_ourprice").get_text()
+	converted_price = float(price[0:5])
 
-print(title.strip())
+	if(converted_price < ): # Enter the price drop after you want to be notifed
+	    send_mail()
+
+	print(converted_price)
+	print(title.strip())
+
+	if(converted_price < ): # Enter the price drop after you want to be notifed
+	    send_mail()
+
+
+def send_mail():
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+
+	server.login('', '')
+
+	subject = 'Price fell down!'
+	body = 'Check the Amazon link '
+
+	msg = f"Subject: {subject}\n\n{body}"
+
+	server.sendmail(
+		'',
+		'',
+		msg
+	)
+	print('HEY EMAIL HAS BEEN SENT!')
+
+	server.quit()
+
+
+check_price()
+
